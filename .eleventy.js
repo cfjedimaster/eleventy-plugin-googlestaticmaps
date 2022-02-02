f1ce0da0725821e197f00664d43443f38288c8e3
Raymond Camden <raymondcamden@gmail.com>
2022-02-02 13:25:25 -0600

    extra quote

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = (eleventyConfig, options) => {
 	}
 
 	eleventyConfig.addShortcode("staticmap", function(address, width=500, height=500, zoom=13, maptype="roadmap") {
-		return `https://maps.googleapis.com/maps/api/staticmap?center=${encodeURIComponent(address)}&zoom=${zoom}&size=${width}x${height}&maptype=${maptype}&key=${options.key}"`;
+		return `https://maps.googleapis.com/maps/api/staticmap?center=${encodeURIComponent(address)}&zoom=${zoom}&size=${width}x${height}&maptype=${maptype}&key=${options.key}`;
 	});
 
 }
